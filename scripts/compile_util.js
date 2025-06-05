50a85a0b36776bf86c57bab8c97c14cfc0c544fa
z-zeechung <zhouzeechung@qq.com>
2025-06-05 21:27:46 +0800

    -

--- scripts/compile_util.js
+++ scripts/compile_util.js
@@ -20,36 +20,36 @@ const config = {
       'node_util': 'shims/node-util-0.12.4/util.js',
       'inherits': 'inherits/inherits_browser.js',
 
-      'internal/util/inspect': 'node/lib/internal/util/inspect.js',
-        'internal/util$': 'shims/util/internal.util.js',
-        'internal/errors': 'shims/util/errors.js',
-        'internal/util/types': 'node/lib/internal/util/types.js',
-          'internal/crypto/keys': 'shims/util/crypto.js',
-        'internal/assert': 'node/lib/internal/assert.js',
-          // 'internal/errors'
-        'internal/bootstrap/realm': 'shims/util/realm.js',
-        'internal/validators': 'shims/util/validators.js',
-        'internal/url': 'shims/util/internal.url.js',
+      // 'internal/util/inspect': 'node/lib/internal/util/inspect.js',
+      //   'internal/util$': 'shims/util/internal.util.js',
+      //   'internal/errors': 'shims/util/errors.js',
+      //   'internal/util/types': 'node/lib/internal/util/types.js',
+      //     'internal/crypto/keys': 'shims/util/crypto.js',
+      //   'internal/assert': 'node/lib/internal/assert.js',
+      //     // 'internal/errors'
+      //   'internal/bootstrap/realm': 'shims/util/realm.js',
+      //   'internal/validators': 'shims/util/validators.js',
+      //   'internal/url': 'shims/util/internal.url.js',
 
-        'internalBinding': 'shims/util/internalBinding.js'
+      //   'internalBinding': 'shims/util/internalBinding.js'
     }
   },
-  module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: [
-            path.resolve('./shims') 
-          ],
-          use: [
-            {
-              loader: path.resolve(__dirname, 'inject-internalBinding-loader.js'),
-            },
-          ],
-          enforce: 'pre',
-        },
-      ],
-    },
+  // module: {
+  //     rules: [
+  //       {
+  //         test: /\.js$/,
+  //         exclude: [
+  //           path.resolve('./shims') 
+  //         ],
+  //         use: [
+  //           {
+  //             loader: path.resolve(__dirname, 'inject-internalBinding-loader.js'),
+  //           },
+  //         ],
+  //         enforce: 'pre',
+  //       },
+  //     ],
+  //   },
 };
 
 webpack(config, (err, stats) => {
